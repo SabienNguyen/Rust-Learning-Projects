5c396564ac760889bd552d46bd4e1986611ca216
SabienNguyen <sabien.q.nguyen@gmail.com>
2025-01-15 15:17:38 -0800

    add erorr handling for text-analyzer and add weather-scraper

--- text-analyzer/src/main.rs
+++ text-analyzer/src/main.rs
@@ -1,8 +1,6 @@
-use std::{collections::HashMap, env, fs};
-fn main() {
-    let arg: Vec<String> = env::args().collect();
-    let file_path = &arg[1];
-    let file = fs::read_to_string(file_path).expect("Should have been able to read the file");
+use std::{collections::HashMap, env, error::Error, fs};
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = read_file()?;
 
     let line_count = get_line_count(&file);
     println!("number of lines in file: {}", line_count);
@@ -11,9 +9,22 @@ fn main() {
     println!("number of words in file: {}", word_count);
 
     let freqs = get_frequency_unique_word(&file);
+    
+    println!("Unique word frequency list:");
     for (word, count) in freqs.iter() {
         println!("{}: {}", word, count);
-    } 
+    }
+    Ok(())
+}
+
+fn read_file() -> Result<String, Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        return Err("Usage: <program_name> <file_path>".into());
+    }
+    let file_path = &args[1];
+    let file = fs::read_to_string(file_path)?;
+    Ok(file)
 }
 
 fn get_line_count(file: &String) -> i64 {
